9b3bb11979316e8c75fbb217f2d5f6dbe8c0ac42
Joberth <joberthdavid@hotmail.com>
2020-07-18 13:31:43 -0300

    Admin magic

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from eventex.subscriptions.models import Subscription
+from django.utils.timezone import now
 
-# Register your models here.
+
+class SubscriptionModelAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'cpf', 'email', 'phone', 'created_at', 'subscribed_today')
+    date_hierarchy = 'created_at'
+    search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
+    list_filter = ('created_at',)
+
+    def subscribed_today(self, obj):
+        return obj.created_at == now().date()
+
+    subscribed_today.short_description = 'Inscrito hoje?'
+    subscribed_today.boolean = True
+
+admin.site.register(Subscription, SubscriptionModelAdmin)
